775e06b7619e2c1249e899ee34dc273f3b6db71f
Al Ardosa <alardosa@gmail.com>
2012-06-18 11:07:38 +0800

    update index.php in application/views/pages

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -3,7 +3,7 @@
 		<tr>
 			<td>
 	<table border="0" width="900" id="table6">
-	<tr><td align="center"><font face="Verdana" style="font-size: 20pt" color="#FFFFFF"><a href="#">HOT DEALS</a> | <a href="#">Back to School</a> | <a href="http://lazadapromo.com/rose_test/index.php/submit">Submit Entry</a> | <a href="#">Gallery</a></font></td></tr>
+	<tr><td align="center"><font face="Verdana" style="font-size: 20pt" color="#FFFFFF"><a href="#">Hot Deals</a> | <a href="#">Back to School</a> | <a href="<?=base_url().'index.php/submit'?>">Submit Entry</a> | <a href="<?=base_url().'index.php/gallery'?>">Gallery</a></font></td></tr>
 		<tr>
 			<td>
 			<table border="0" width="100%" id="table7" bgcolor="#FA9738">
@@ -104,12 +104,12 @@
 	<p>&nbsp;</p>
 	<p align="center"><font face="Arial" size="1">
 	<font class="ws8" color="#000000">Copyright (c) Lazada.com.ph -
-	<a href="#">Lazada.com.ph</a> | 
+	<a href="http://www.lazada.com.ph/">Lazada.com.ph</a> | 
 	</font>
 	<font class="ws8">
-	<a href="#">Learn More</a></font><font class="ws8" color="#000000"> 
+	<a href="#">Hot Deals</a></font><font class="ws8" color="#000000"> 
 	| </font><font class="ws8">
-	<a href="#">Privacy</a></font><font class="ws8" color="#000000">  
+	<a href="#">Back to School</a></font><font class="ws8" color="#000000">  
 	|&nbsp; <a href="#">Disclaimer</a> 
 	| </font><font class="ws8">
 	<a href="#">Contact Us</a></font></font><br></div>
