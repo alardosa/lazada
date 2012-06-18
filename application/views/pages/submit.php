84b12dbc28e3621bd24fc37480e5a03d32992853
rosann <reclavea@gmail.com>
2012-06-18 15:00:01 +0800

    added .borderWrap

--- application/views/pages/submit.php
+++ application/views/pages/submit.php
@@ -1,20 +1,25 @@
 <div id="mainWrap">
-<br /><br />
-<font face="Verdana" style="font-size: 20pt" color="#FFFFFF"><a href="#">HOT DEALS</a> | <a href="#">Back to School</a> | <a href="#">Submit Entry</a> | <a href="http://lazadapromo.com/rose_test/index.php/gallery">Gallery</a></font>
-<br /><br />
-<?php echo $this->load->view('partials/notices'); ?>
 
-<?php echo form_open_multipart(site_url('submit'),'class="form"');
-	echo form_hidden('user_id',$this->session->userdata('user_id'));
-	echo '<div class="inpWrap"><label>Title</label>'.form_input('title',$txtfield->title).'</div>';
-	echo '<div class="inpWrap"><label>Description</label>'. form_textarea('description',$txtfield->description).'</div>';
-	echo '<div class="inpWrap"><label>Upload file</label>'. form_upload('userfile','','class="file_upload"').'</div>';
-	
-	//added
-	echo img('image/securimage', TRUE).'<br />';
-	echo '<div class="inpWrap"><label>Enter Captcha</label>'.form_input('captcha',$txtfield->captcha).'</div>';
+<div class="borderWrap">
 
-	echo form_submit('submit','Submit');
+	<br /><br />
+	<font face="Verdana" style="font-size: 20pt" color="#FFFFFF"><a href="#">HOT DEALS</a> | <a href="#">Back to School</a> | <a href="#">Submit Entry</a> | <a href="http://lazadapromo.com/rose_test/index.php/gallery">Gallery</a></font>
+	<br /><br />
+	<?php echo $this->load->view('partials/notices'); ?>
 	
-echo form_close();?>
-</div>	
+	<?php echo form_open_multipart(site_url('submit'),'class="form"');
+		echo form_hidden('user_id',$this->session->userdata('user_id'));
+		echo '<div class="inpWrap"><label>Title</label>'.form_input('title',$txtfield->title).'</div>';
+		echo '<div class="inpWrap"><label>Description</label>'. form_textarea('description',$txtfield->description).'</div>';
+		echo '<div class="inpWrap"><label>Upload file</label>'. form_upload('userfile','','class="file_upload"').'</div>';
+		
+		//added
+		echo '<div class="inpWrap"><label>&nbsp;</label>'.img('image/securimage', TRUE).'</div>';
+		echo '<div class="inpWrap"><label>Enter Captcha</label>'.form_input('captcha',$txtfield->captcha).'</div>';
+	
+		echo form_submit('submit','Submit');
+		
+	echo form_close();?>
+	</div>	
+
+</div><!--.borderWrap-->
