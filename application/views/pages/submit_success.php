714da18cfe366e0004067494a57130aeb9f9c658
Al Ardosa <alardosa@gmail.com>
2012-06-18 11:42:00 +0800

    update application/views/pages submit_success.php

--- application/views/pages/submit_success.php
+++ application/views/pages/submit_success.php
@@ -1,5 +1,39 @@
+<div align="center">
+	<table border="1" id="table1" bordercolor="#5A8AC8" style="border-collapse: collapse" cellspacing="5" cellpadding="3">
+		<tr>
+			<td>
+	<table border="0" width="900" id="table6">
+	<tr><td align="center"><font face="Verdana" style="font-size: 20pt" color="#FFFFFF">><a href="<?=base_url()?>">Home</a> | <a href="#">Hot Deals</a> | <a href="#">Back to School</a> | <a href="<?=base_url().'index.php/submit'?>">Submit Entry</a> | <a href="<?=base_url().'index.php/gallery'?>">Gallery</a></font></td></tr>
+		<tr>
+			<td>
+			<table border="0" width="100%" id="table11">
+				<tr>
+					<td>
 <div id="mainWrap">
 <br /><br />
 <h1>Thank you for submitting your entry</h1>
-<a href="<?php echo site_url('gallery'); ?>">Go back to gallery</a>
+<br /><br />
 </div>
+
+			
+			        </td>
+				</tr>
+			</table>
+			</td>
+		</tr>
+	</table>
+			</td>
+		</tr>
+	</table>
+	<p>&nbsp;</p>
+	<p align="center"><font face="Arial" size="1">
+	<font class="ws8" color="#000000">Copyright (c) Lazada.com.ph -
+	<a href="http://www.lazada.com.ph/">Lazada.com.ph</a> | 
+	</font>
+	<font class="ws8">
+	<a href="#">Hot Deals</a></font><font class="ws8" color="#000000"> 
+	| </font><font class="ws8">
+	<a href="#">Back to School</a></font><font class="ws8" color="#000000">  
+	|&nbsp; <a href="#">Disclaimer</a> 
+	| </font><font class="ws8">
+	<a href="#">Contact Us</a></font></font><br></div>
