d5fa7f743628ddf8ae91b8731664db6eb4ffd7b5
rosann <reclavea@gmail.com>
2012-06-18 10:46:07 +0800

    update image viewing iframe to separate page

--- application/views/pages/gallery.php
+++ application/views/pages/gallery.php
@@ -2,9 +2,11 @@
 	<p><?php echo anchor('submit','Submit Entry'); ?></p>
 	<ul class="gallery">
 <?php foreach($images as $file):?>
-	<li><a href="<?=site_url('gallery/view/'.$file->id) ?>" class="iframe"><img src="<?=site_url('files/thumb/'.$file->id.'/150/150');?>" /></a><br />
-	<div class="fb-like" data-href="<?php echo site_url('gallery/view/'.$file->id)?>" data-send="false" data-layout="button_count" data-width="95" data-show-faces="false"></div>
-	<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en" data-count="none" data-counturl="<?=site_url('gallery/view/'.$file->id)?>" data-url="<?=site_url('gallery/view/'.$file->id )?>" data-text="Follow @JersonelsA @angelprisz">Tweet</a>
+	<li><a href="<?=site_url('gallery/view/'.$file->id) ?>" target="_blank"><img src="<?=site_url('files/thumb/'.$file->id.'/150/150');?>" /></a><br />
+	
+	<div class="fb-like" data-href="<?php echo site_url('gallery/view/'.$file->id)?>" data-send="false" data-layout="button_count" data-width="95" data-show-faces="false" style="vertical-align:top; zoom:1; *display:inline"></div>
+	
+	<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en" data-count="horizontal" data-counturl="<?=site_url('gallery/view/'.$file->id)?>" data-url="<?=site_url('gallery/view/'.$file->id )?>" data-text="Follow @JersonelsA @angelprisz">Tweet</a>
 
 	</li>
 	
