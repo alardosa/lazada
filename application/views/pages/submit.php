778a49e958ebb3d78153cdd0825b65df9b0f9947
Al Ardosa <alardosa@gmail.com>
2012-06-18 15:25:35 +0800

    UPDATED submit.php application/views/pages

--- application/views/pages/submit.php
+++ application/views/pages/submit.php
@@ -1,10 +1,16 @@
+<div align="center">
+	<table border="1" id="table1" bordercolor="#5A8AC8" style="border-collapse: collapse" cellspacing="5" cellpadding="3">
+		<tr>
+			<td>
+	<table border="0" width="900" id="table6">
+	<tr><td align="center"><font face="Verdana" style="font-size: 20pt" color="#FFFFFF">><a href="<?=base_url()?>">Home</a> | <a href="#">Hot Deals</a> | <a href="#">Back to School</a> | <a href="<?=base_url().'index.php/submit'?>">Submit Entry</a> | <a href="<?=base_url().'index.php/gallery'?>">Gallery</a></font></td></tr>
+		<tr>
+			<td>
+			<table border="0" width="100%" id="table11">
+				<tr>
+					<td>
 <div id="mainWrap">
-
-<div class="borderWrap">
-
-	<br /><br />
-	<font face="Verdana" style="font-size: 20pt" color="#FFFFFF"><a href="#">HOT DEALS</a> | <a href="#">Back to School</a> | <a href="#">Submit Entry</a> | <a href="http://lazadapromo.com/rose_test/index.php/gallery">Gallery</a></font>
-	<br /><br />
+<br /><br />
 	<?php echo $this->load->view('partials/notices'); ?>
 	
 	<?php echo form_open_multipart(site_url('submit'),'class="form"');
@@ -20,6 +26,28 @@
 		echo form_submit('submit','Submit');
 		
 	echo form_close();?>
-	</div>	
+<br /><br />
+</div>
 
-</div><!--.borderWrap-->
+			
+			        </td>
+				</tr>
+			</table>
+			</td>
+		</tr>
+	</table>
+			</td>
+		</tr>
+	</table>
+	<p>&nbsp;</p>
+	<p align="center"><font face="Arial" size="1">
+	<font class="ws8" color="#000000">Copyright (c) Lazada.com.ph -
+	<a href="http://www.lazada.com.ph/">Lazada.com.ph</a> | 
+	</font>
+	<font class="ws8">
+	<a href="#">Hot Deals</a></font><font class="ws8" color="#000000"> 
+	| </font><font class="ws8">
+	<a href="#">Back to School</a></font><font class="ws8" color="#000000">  
+	|&nbsp; <a href="#">Disclaimer</a> 
+	| </font><font class="ws8">
+	<a href="#">Contact Us</a></font></font><br></div>
