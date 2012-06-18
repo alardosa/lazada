70180cc9bfafc3a11b4356026ffa2ae10640a134
rosann <reclavea@gmail.com>
2012-06-18 13:54:09 +0800

    update tag class

--- application/views/partials/notices.php
+++ application/views/partials/notices.php
@@ -1,13 +1,13 @@
 <?php
 	if(validation_errors()):
-		echo '<div class="notification errors closable">'.validation_errors().'</div>';
+		echo '<div class="notification errors">'.validation_errors().'</div>';
 	endif;
 	
 	if($this->session->flashdata('success')):
-		echo '<div class="notification success closable">'.$this->session->flashdata('success').'</div>';
+		echo '<div class="notification success">'.$this->session->flashdata('success').'</div>';
 	endif;
 	
 	if($this->session->flashdata('error')):
-		echo '<div class="notification errors closable">'.$this->session->flashdata('error').'</div>';
+		echo '<div class="notification errors">'.$this->session->flashdata('error').'</div>';
 	endif;
 ?>
