97a53088022ec2edd42a51bba1408d6bac3b2836
rosann <reclavea@gmail.com>
2012-06-18 13:54:38 +0800

    update submit form

--- application/views/pages/submit.php
+++ application/views/pages/submit.php
@@ -2,23 +2,18 @@
 <br /><br />
 <font face="Verdana" style="font-size: 20pt" color="#FFFFFF"><a href="#">HOT DEALS</a> | <a href="#">Back to School</a> | <a href="#">Submit Entry</a> | <a href="http://lazadapromo.com/rose_test/index.php/gallery">Gallery</a></font>
 <br /><br />
-<?php if(validation_errors()){
-	echo validation_errors();	
-}
-	if($error){
-		echo '<p>'.$error.'</p>';
-	}
-?>
+<?php echo $this->load->view('partials/notices'); ?>
+
 <?php echo form_open_multipart(site_url('submit'),'class="form"');
 	echo form_hidden('user_id',$this->session->userdata('user_id'));
-	echo 'Title '.form_input('title',$txtfield->title).'<br />';
-	echo 'Description'. form_textarea('description',$txtfield->description).'<br />';
-	echo 'Upload file'. form_upload('userfile').'<br />';
+	echo '<div class="inpWrap"><label>Title</label>'.form_input('title',$txtfield->title).'</div>';
+	echo '<div class="inpWrap"><label>Description</label>'. form_textarea('description',$txtfield->description).'</div>';
+	echo '<div class="inpWrap"><label>Upload file</label>'. form_upload('userfile','','class="file_upload"').'</div>';
 	
 	//added
 	echo img('image/securimage', TRUE).'<br />';
-	echo 'Enter Captcha '.form_input('captcha',$txtfield->captcha).'<br />';
-	
+	echo '<div class="inpWrap"><label>Enter Captcha</label>'.form_input('captcha',$txtfield->captcha).'</div>';
+
 	echo form_submit('submit','Submit');
 	
 echo form_close();?>
