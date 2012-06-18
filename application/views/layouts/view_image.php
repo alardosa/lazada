7b1382b112be7e2c8c759f7e0936270e9b1f7770
Al Ardosa <alardosa@gmail.com>
2012-06-18 11:28:28 +0800

    Update views/layouts and style.css

---
+++ application/views/layouts/view_image.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" 
+	xmlns:fb="http://www.facebook.com/2008/fbml">
+	<head>
+	<title>Lazada Photo Contest</title>
+	<!--<link rel="stylesheet" href="<?=CSS?>reset.css" />-->
+	<link rel="stylesheet" href="<?=CSS?>colorbox.css" />
+	<link rel="stylesheet" href="<?=CSS?>style.css" />
+	<?php echo $template['metadata']; ?>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script type="text/javascript" src="<?=JS?>jquery.colorbox.js"></script>	
+	<script type="text/javascript" src="<?=JS?>colorbox_script.js"></script>	
+		<div id="fb-root"></div>
+		
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=319607828121764";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+		
+		<script>
+		    window.fbAsyncInit = function() {
+				
+				/* for liking the image */
+				$(document).ready(function(){
+					FB.Event.subscribe('edge.create', function (href, widget) {
+						var str = href;
+						//alert (str);
+						$.post("http://lazadapromo.com/jers_test/check/check.php",{ url: str },
+				        function(data) {
+				            $('#feedback').html(data);
+				        });
+					});
+				});
+				
+				/* for unliking the image */
+		    	$(document).ready(function(){
+		        	FB.Event.subscribe('edge.remove', function (href, widget) {
+		            	var str = href;
+						//alert (str);
+						$.post("http://lazadapromo.com/jers_test/check/check.php",{ url: str },
+				        function(data) {
+				            $('#feedback').html(data);
+				        });
+					});   
+		        });
+		    };
+		
+		    (function(d) {
+		        var js, id = 'facebook-jssdk'; if (d.getElementById(id)) { return; }
+		        js = d.createElement('script'); js.id = id; js.async = true;
+		        js.src = "//connect.facebook.net/en_US/all.js";
+		        d.getElementsByTagName('head')[0].appendChild(js);
+		    }(document));
+		</script>
+		
+		<!-- twitter -->
+		<script>
+		!function(d,s,id){
+			var js,fjs=d.getElementsByTagName(s)[0];
+			if(!d.getElementById(id)){
+				js=d.createElement(s);
+				js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);
+		}}(document,"script","twitter-wjs");
+		</script>
+		<!-- twitter -->
+	</head>
+	
+	<body>
+		<?php echo $template['body'];?>
+		<div id="feedback"></div>
+	</body>
+</html>
--- application/views/pages/view_image.php
+++ application/views/pages/view_image.php
@@ -1,15 +1,28 @@
-<div class="viewImageWrap">
+<div align="center">
+	<table border="1" id="table1" bordercolor="#5A8AC8" style="border-collapse: collapse" cellspacing="5" cellpadding="3">
+		<tr>
+			<td>
+	<table border="0" width="900" id="table6">
+	<tr><td align="center"><font face="Verdana" style="font-size: 20pt" color="#FFFFFF">><a href="<?=base_url()?>">Home</a> | <a href="#">Hot Deals</a> | <a href="#">Back to School</a> | <a href="<?=base_url().'index.php/submit'?>">Submit Entry</a> | <a href="<?=base_url().'index.php/gallery'?>">Gallery</a></font></td></tr>
+		<tr>
+			<td>
+			<table border="0" width="100%" id="table11">
+				<tr>
+					<td>
+			
+			
+			<div class="viewImageWrap" align="center">
 <?php if($file->status == 1):?>
 <p><img src="<?php echo site_url('files/large/'.$file->id); ?>" height="<?=$file->height?>" width="<?=$file->width?>" /></p>
 
 <div class="fb-like" data-href="<?php echo site_url('gallery/view/'.$file->id)?>" data-send="false" data-layout="button_count" data-width="200" data-show-faces="false"></div>
 
-<p><?php echo $file->title ?></p>
-<p><?php echo $file->description ?></p>
+<p>Title: <?php echo $file->title ?></p>
+<p>Description: <?php echo $file->description ?></p>
 
 <div>
 <p>
-<img src="http://graph.facebook.com/<?=$file->user_id?>/picture" height="38" />
+<img src="http://graph.facebook.com/<?=$file->user_id?>/picture" height="38" /><br />
 <strong>Submitted by:</strong> <?php echo $file->first_name.' '.$file->last_name ?></p>
 </div>
 
@@ -19,3 +32,26 @@ else:
 redirect('gallery');
 endif; ?>
 </div>
+			
+			
+			        </td>
+				</tr>
+			</table>
+			</td>
+		</tr>
+	</table>
+			</td>
+		</tr>
+	</table>
+	<p>&nbsp;</p>
+	<p align="center"><font face="Arial" size="1">
+	<font class="ws8" color="#000000">Copyright (c) Lazada.com.ph -
+	<a href="http://www.lazada.com.ph/">Lazada.com.ph</a> | 
+	</font>
+	<font class="ws8">
+	<a href="#">Hot Deals</a></font><font class="ws8" color="#000000"> 
+	| </font><font class="ws8">
+	<a href="#">Back to School</a></font><font class="ws8" color="#000000">  
+	|&nbsp; <a href="#">Disclaimer</a> 
+	| </font><font class="ws8">
+	<a href="#">Contact Us</a></font></font><br></div>
